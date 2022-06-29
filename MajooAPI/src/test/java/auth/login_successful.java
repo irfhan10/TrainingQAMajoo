58101f3956ae85bdfd7b507861ad573874b07efc
irfhan10 <48820448+irfhan10@users.noreply.github.com>
2022-06-29 21:45:46 +0700

    Add files via upload
    update

--- MajooAPI/src/test/java/auth/login_successful.java
+++ MajooAPI/src/test/java/auth/login_successful.java
@@ -1,11 +1,14 @@
 package auth;
 
 import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
 import org.testng.ITestContext;
 import org.testng.annotations.Test;
+
+import java.io.File;
 
 import static io.restassured.RestAssured.given;
 
@@ -23,7 +26,11 @@ public class login_successful {
         request.header("Content-Type", "application/json");
 
         Response response = request.post("/api/login");
-        response.then().assertThat().statusCode(200);
+        String schemaPath = "src/resources/login_successful.json";
+
+        response.then().assertThat()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File(schemaPath)));
         System.out.println(response.asString());
     }
 }
